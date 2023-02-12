fdc45df28e39462787d89a37b81ba4d9a8f828a5
Andre Espinoza <ae95caba@gmail.com>
2023-02-12 06:15:49 -0300

    delete unnesesary game.js component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Game } from "./Game";
+import "./App.scss";
 import "./index.scss";
+import { Level } from "./components/level";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Level />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
