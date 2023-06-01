7450b8ab0f48520a25bca10e7283207f4e5a5177
Andre Espinoza <ae95caba@gmail.com>
2023-06-01 14:33:34 -0300

    commit necesary to change branch:

--- memory-card/src/components/card.js
+++
@@ -1,8 +0,0 @@
-export function Card(props) {
-  return (
-    <div className="card" onClick={props.onClick}>
-      <img src={props.waifu.url} alt={props.waifu.alt} />
-      <p>{props.waifu.name}</p>
-    </div>
-  );
-}
